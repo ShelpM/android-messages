54f86cf90e89e36b3e5e4dbfc8ea254bb7c21c2c
Shelp <contact@alangueite.fr>
2019-01-24 01:14:47 +0100

    v1.0.0

---
+++ main.js
@@ -0,0 +1,61 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+process.env.NODE_ENV = 'production';
+
+let mainWindow;
+let addWindow;
+
+app.on('ready', function(){
+    
+    mainWindow = new BrowserWindow({});
+    
+    mainWindow.loadURL(url.format({
+      pathname: path.join(messages.android.com),
+      protocol: 'http:',
+      slashes:true
+    }));
+    
+    mainWindow.on('closed', function(){
+      app.quit();
+    });
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const mainMenuTemplate = [
+    {  
+        label: '',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : " Ctrl+Q",
+        click(){
+            app.quit();
+        }
+    }
+    
+];
+
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+      label: 'Developer Tools',
+      submenu:[
+        {
+          role: 'reload'
+        },
+        {
+          label: 'Toggle DevTools',
+          accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+          click(item, focusedWindow){
+            focusedWindow.toggleDevTools();
+          }
+        }
+      ]
+    });
+}
